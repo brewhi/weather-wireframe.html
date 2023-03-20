9adee34e5c15b9a58f3087ac1fcc7651939605cd
brewhi <126518688+brewhi@users.noreply.github.com>
2023-03-19 21:37:12 -0400

    city temp search from imperial to metric

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ function showWeather(response) {
 
 function searchCity(city) {
   let apiKey = "2ff29bed3181c3526c35cc5408037f85";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeather);
 }
 
