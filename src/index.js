e9446ae086d3ddbd1dc934814836c4e16a0d9c97
brewhi <126518688+brewhi@users.noreply.github.com>
2023-03-22 14:21:58 -0400

    fixed code to position c | f toggle

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
-let now = new Date();
+function formateDate() {
+  let now = new Date();
 
-let today = document.querySelector("p.today");
+  let today = document.querySelector("p.today");
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
 
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  today.innerHTML = `${day} ${hours}:${minutes}`;
 }
-
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
-today.innerHTML = `${day} ${hours}:${minutes}`;
 
 function showWeather(response) {
   let h2 = document.querySelector("h2");
