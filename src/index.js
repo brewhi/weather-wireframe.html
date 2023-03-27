fc1bca687fe9ddfdc7a1388b3aae7f0959a0f1f0
brewhi <126518688+brewhi@users.noreply.github.com>
2023-03-26 20:02:11 -0400

    function showWeather update
    for date display

--- src/index.js
+++ src/index.js
@@ -35,6 +35,7 @@ function showWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let iconElement = document.querySelector("#icon");
+  let today = document.querySelector("p.today");
 
   celciusTemperature = response.data.main.temp;
 
@@ -43,6 +44,7 @@ function showWeather(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  today.innerHTML = formateDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
